f790ea95e8dfdde3ba9fc04d62ae08ad5ed0036f
Rahul Suvarna <dsrahul@yahoo.com>
2018-01-01 20:58:43 +0000

    ubuntu installation success

--- src/test/java/steps/AbstractSpringTests.java
+++ src/test/java/steps/AbstractSpringTests.java
@@ -4,11 +4,12 @@ import com.standard.SpringBootWebApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = SpringBootWebApplication.class, loader = SpringBootContextLoader.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 public abstract class AbstractSpringTests {
 }
